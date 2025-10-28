70826786a286f328711c1c8abfd2717bf800b212
JorG <jorge.escobar.2004@gmail.com>
2025-10-28 10:43:57 -0400

    Ahora hay funcionalidad basica para asegurarse de que la logica de cifrado y descifrado es correcta

--- src/com/jorge/caesar/Cipher.java
+++ src/com/jorge/caesar/Cipher.java
@@ -1,4 +1,59 @@
 package com.jorge.caesar;
 
 public class Cipher {
+    private char[] alphabet;
+
+    // Constructor que recibe el alfabeto
+    public Cipher(char[] alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    // Método para cifrar texto
+    public String encrypt(String text, int shift) {
+        StringBuilder encrypted = new StringBuilder();
+
+        for (char c : text.toCharArray()) {
+            char upper = Character.toUpperCase(c);
+            int index = indexOf(upper);
+
+            if (index != -1) {
+                int newIndex = (index + shift) % alphabet.length;
+                encrypted.append(alphabet[newIndex]);
+            } else {
+                // Si el carácter no está en el alfabeto, se deja igual
+                encrypted.append(c);
+            }
+        }
+
+        return encrypted.toString();
+    }
+
+    // Método para descifrar texto
+    public String decrypt(String encryptedText, int shift) {
+        StringBuilder decrypted = new StringBuilder();
+
+        for (char c : encryptedText.toCharArray()) {
+            char upper = Character.toUpperCase(c);
+            int index = indexOf(upper);
+
+            if (index != -1) {
+                int newIndex = (index - shift + alphabet.length) % alphabet.length;
+                decrypted.append(alphabet[newIndex]);
+            } else {
+                // Si no está en el alfabeto, lo deja igual
+                decrypted.append(c);
+            }
+        }
+        return decrypted.toString();
+    }
+
+    // Método auxiliar: busca la posición del carácter en el alfabeto
+    private int indexOf(char c) {
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == c) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
--- src/com/jorge/caesar/MainApp.java
+++ src/com/jorge/caesar/MainApp.java
@@ -1,7 +1,18 @@
 package com.jorge.caesar;
 
 public class MainApp {
-    public static void main(String[] args){
-        System.out.println("Holaa Mundo Caesar Cipher");
+    public static void main(String[] args) {
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        Cipher cipher = new Cipher(alphabet);
+
+        String text = "HOLA MUNDO";
+        int shift = 3;
+
+        String encrypted = cipher.encrypt(text, shift);
+        String decrypted = cipher.decrypt(encrypted, shift);
+
+        System.out.println("Texto original: " + text);
+        System.out.println("Texto cifrado: " + encrypted);
+        System.out.println("Texto descifrado: " + decrypted);
     }
 }
